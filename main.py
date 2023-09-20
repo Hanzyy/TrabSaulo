1089f9ffe20b436389593976734b7f55113ed074
Dellao <47403362+Hanzyy@users.noreply.github.com>
2023-09-20 10:33:13 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+from listaSimplesmenteEncadeadaComIteradorFinal_Solucao import SinglyLinkedListIterator
+from array_stack import ArrayStack
+from array_queue_new import ArrayQueue
+from listaDuplamenteEncadeadaComIteradorFinal_Solucao import DoublyLinkedListIterator
+
+def rodoviasCidade(nomeCidade, lstRodovias):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
